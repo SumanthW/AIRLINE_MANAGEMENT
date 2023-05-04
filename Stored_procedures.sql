0d193753123ae9ec6f5ca9d8a955d51e8103b8e1
SumanthW <128551121+SumanthW@users.noreply.github.com>
2023-05-04 19:48:49 -0400

    Update Stored_procedures.sql

--- Stored_procedures.sql
+++ Stored_procedures.sql
@@ -88,14 +88,41 @@ AS
 BEGIN
 END DEPART_AC;
 /
-CREATE OR REPLACE PROCEDURE SET_FUEL_PRICE()
+
+CREATE OR REPLACE PROCEDURE SET_FUEL_PRICE
 AS
+    v_UPPER_LIMIT number;
+    v_LOWER_LIMIT number;
 BEGIN
+    v_UPPER_LIMIT := 2500;
+    v_LOWER_LIMIT := 250;
+    
+    INSERT INTO FUEL_PRICE values (trunc(dbms_random.value(1,100)*(v_UPPER_LIMIT/100)+v_LOWER_LIMIT),systimestamp AT TIME ZONE 'AMERICA/NEW_YORK');
+    DELETE FROM FUEL_PRICE where AT_TIME < systimestamp AT TIME ZONE 'AMERICA/NEW_YORK' - 1;
+    COMMIT;
+
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.put_line('Oops! Some fault with generation!');
+        ROLLBACK;
 END SET_FUEL_PRICE;
 /
 
-CREATE OR REPLACE PROCEDURE SET_CO2_PRICE()
+CREATE OR REPLACE PROCEDURE SET_CO2_PRICE
 AS
+    v_UPPER_LIMIT number;
+    v_LOWER_LIMIT number;
 BEGIN
+    v_UPPER_LIMIT := 300;
+    v_LOWER_LIMIT := 100;
+    
+    INSERT INTO CO2_PRICE values (trunc(dbms_random.value(1,100)*(v_UPPER_LIMIT/100)+v_LOWER_LIMIT),systimestamp AT TIME ZONE 'AMERICA/NEW_YORK');
+    DELETE FROM CO2_PRICE where AT_TIME < systimestamp AT TIME ZONE 'AMERICA/NEW_YORK' - 1;
+    COMMIT;
+
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.put_line('Oops! Some fault with generation!');
+        ROLLBACK;
 END SET_CO2_PRICE;
 /
